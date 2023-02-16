84d2c6233a4cf34ebc7d19aa0e5910cd847074a0
dzl <330329746@qq.com>
2023-02-17 02:14:11 +0800

    主分支第二次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,8 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello hot-fix!");
         System.out.println("hello master test!");
+        System.out.println("hello master test!");
+        System.out.println("hello master test!");
     }
 }
 
