d63a3c578f1c1953c9f610499d784772d7b138b8
ZhlLinDong <50834365+ZhlLinDong@users.noreply.github.com>
2023-02-17 02:33:16 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("主分支修改");
         System.out.println("修复补丁修改");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
 
