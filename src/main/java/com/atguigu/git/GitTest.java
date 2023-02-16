1359df10d9cf7e33787ebb0e6837261d9b28430b
dzl <330329746@qq.com>
2023-02-17 02:15:38 +0800

    修复补丁第一次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,9 +6,7 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello hot-fix!");
-        System.out.println("hello master test!");
-        System.out.println("hello master test!");
-        System.out.println("hello master test!");
+
     }
 }
 
